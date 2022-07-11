23c54c9587698db85c12fce538c560e59c99af8a
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-07-11 10:55:59 +0200

    Démarrer le serveur backend

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Zazuu9:Namaste85@cluster0.qpibc.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+module.exports = app;
