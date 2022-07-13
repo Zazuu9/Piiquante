563bb13eacdb64e31d7f1b09ad502240b944774c
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-07-13 18:30:50 +0200

    Ajout des requetes api

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
+const path = require('path');
+const app = express();
+
 
 mongoose.connect('mongodb+srv://Zazuu9:Namaste85@cluster0.qpibc.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-const app = express();
-
-app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,6 +19,11 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.json());
+
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 module.exports = app;
 
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,9 +1,9 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
+       
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
@@ -17,17 +17,17 @@ exports.signup = (req, res, next) => {
     .catch(error => res.status(500).json({error}))
 };
 
-exports.login = (req, res ,next) => {
-    User.findOne({email: req.body.email})
+exports.login = (req, res, next) => {
+    User.findOne({email:req.body.email})
     .then(user => {
         if (user === null) {
-            res.status(401).json('Identifiant/mot de passe incorrect')
-        } else {
+            res.status(401).json({message: 'Paire identifiant/mot de passe incorrect'});
+        }else{
             bcrypt.compare(req.body.password, user.password)
             .then(valid => {
-                if (!valid){
-                    res.status(401).json('Identifiant/mot de passe incorrect')
-                } else {
+                if (!valid) {
+                    res.status(401).json({message: 'Paire identifiant/mot de passe incorrect'});
+                }else{
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
@@ -43,4 +43,3 @@ exports.login = (req, res ,next) => {
     })
     .catch(error => res.status(500).json({error}))
 };
-
