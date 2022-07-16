474263b8fb5e1f974dd15fc0d1d7d0d9f4a49201
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-07-16 11:00:26 +0200

    Update

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -6,16 +6,16 @@ exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
     const sauce = new Sauce({
-      ...sauceObject,
-      imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
-      likes: 0,
-      dislikes: 0,
-      usersLiked: [" "],
-      usersDisliked: [" "],
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [" "],
+    usersDisliked: [" "],
     });
     sauce.save()
-      .then(() =>res.status(201).json(sauce))
-      .catch((error) => res.status(400).json({ error }));
+    .then(() =>res.status(201).json(sauce))
+    .catch((error) => res.status(400).json({ error }));
 };
 
 exports.getAllSauce = (req, res, next) => {
@@ -45,28 +45,25 @@ exports.deleteSauce = (req, res, next) => {
 exports.likeSauce = (req, res, next) => {
     if (req.body.like === 1) {
         Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: req.body.like++ }, $push: { usersLiked: req.body.userId } })
-              .then((sauce) => res.status(200).json({ message: 'Ajout Like' }))
-              .catch(error => res.status(400).json({ error }))
-      } else if (req.body.like === -1) {
+            .then((sauce) => res.status(200).json({ message: 'Ajout Like' }))
+            .catch(error => res.status(400).json({ error }))
+    } else if (req.body.like === -1) {
           Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: (req.body.like++) * -1 }, $push: { usersDisliked: req.body.userId } })
-              .then((sauce) => res.status(200).json({ message: 'Ajout Dislike' }))
-              .catch(error => res.status(400).json({ error }))
-      } else {
-          Sauce.findOne({ _id: req.params.id })
-              .then(sauce => {
-                  if (sauce.usersLiked.includes(req.body.userId)) {
-                      Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
-                          .then((sauce) => { res.status(200).json({ message: 'Suppression Like' }) })
-                          .catch(error => res.status(400).json({ error }))
-                  } else if (sauce.usersDisliked.includes(req.body.userId)) {
-                      Sauce.updateOne({ _id: req.params.id }, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
-                          .then((sauce) => { res.status(200).json({ message: 'Suppression Dislike' }) })
-                          .catch(error => res.status(400).json({ error }))
-                  }
-              })
-              .catch(error => res.status(400).json({ error }))
-      }
+            .then((sauce) => res.status(200).json({ message: 'Ajout Dislike' }))
+            .catch(error => res.status(400).json({ error }))
+    } else {
+        Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            if (sauce.usersLiked.includes(req.body.userId)) {
+                Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
+                    .then((sauce) => { res.status(200).json({ message: 'Suppression Like' }) })
+                    .catch(error => res.status(400).json({ error }))
+            } else if (sauce.usersDisliked.includes(req.body.userId)) {
+                Sauce.updateOne({ _id: req.params.id }, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
+                    .then((sauce) => { res.status(200).json({ message: 'Suppression Dislike' }) })
+                    .catch(error => res.status(400).json({ error }))
+                }
+            })
+        .catch(error => res.status(400).json({ error }))
+    }
 }
-
-
-  
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
-       
     bcrypt.hash(req.body.password, 10)
     .then(hash => {
         const user = new User({
@@ -12,7 +11,7 @@ exports.signup = (req, res, next) => {
         })
         user.save()
         .then(() => res.status(201).json('Utilisateur créé !'))
-        .catch(error => res.status(500).json({error}))
+        .catch(error => res.status(409).json({error}))
     })
     .catch(error => res.status(500).json({error}))
 };
@@ -20,20 +19,20 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
     User.findOne({email:req.body.email})
     .then(user => {
-        if (user === null) {
-            res.status(401).json({message: 'Paire identifiant/mot de passe incorrect'});
+        if (!user) {
+            res.status(401).json({message: 'Utilisateur non trouvé !'});
         }else{
             bcrypt.compare(req.body.password, user.password)
             .then(valid => {
                 if (!valid) {
-                    res.status(401).json({message: 'Paire identifiant/mot de passe incorrect'});
+                    res.status(401).json({message: 'Identifiant/mot de passe incorrect'});
                 }else{
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
-                            {userId: user._id},
-                            'RANDOM_TOKEN_SECRET',
-                            {expiresIn: '24h'}
+                        {userId: user._id},
+                        'RANDOM_TOKEN_SECRET',
+                        {expiresIn: '24h'}
                         )
                     })
                 }
